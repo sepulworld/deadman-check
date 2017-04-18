2075119078e75d75bbb1b306af56a981cf85caa7
zane <zane.williamson@gmail.com>
2017-04-17 23:13:58 -0700

    initial commit to support recursive key path monitoring

--- lib/deadman_check_keyset.rb
+++ lib/deadman_check_keyset.rb
@@ -1,16 +1,18 @@
 require 'deadman_check/version'
 require 'deadman_check_global'
 require 'diplomat'
+require 'json'
 
 module DeadmanCheck
   # KeySet Class
   class KeySet
-    attr_accessor :host, :port, :key
+    attr_accessor :host, :port, :key, :frequency
 
-    def initialize(host, port, key)
+    def initialize(host, port, key, frequency)
       @host = host
       @port = port
       @key  = key
+      @frequency = frequency
     end
 
     def _configure_diplomat(host, port)
@@ -19,15 +21,20 @@ module DeadmanCheck
       end
     end
 
-    def _update_consul_key(host, port, key)
+    def _generate_json(epoch, frequency)
+      consul_key = { :epoch => epoch, :frequency => frequency }
+      consul_key.to_json
+    end
+
+    def _update_consul_key(host, port, key, frequency)
       DeadmanCheck::DeadmanCheckGlobal.new.configure_diplomat(host, port)
       epoch_time_now = DeadmanCheck::DeadmanCheckGlobal.new.get_epoch_time
-      Diplomat::Kv.put(key, "#{epoch_time_now}")
+      Diplomat::Kv.put(key, "#{_generate_json(epoch_time_now, frequency)}")
       puts "Consul key #{key} updated EPOCH to #{epoch_time_now}"
     end
 
     def run_consul_key_update
-      _update_consul_key(@host, @port, @key)
+      _update_consul_key(@host, @port, @key, @frequency)
     end
   end
 end
--- lib/deadman_check_switch.rb
+++ lib/deadman_check_switch.rb
@@ -2,17 +2,17 @@ require 'deadman_check/version'
 require 'deadman_check_global'
 require 'diplomat'
 require 'slack-ruby-client'
+require 'json'
 
 module DeadmanCheck
   # Switch class
   class SwitchMonitor
-    attr_accessor :host, :port, :key, :freshness, :alert_to, :daemon_sleep
+    attr_accessor :host, :port, :key_path, :alert_to, :daemon_sleep
 
-    def initialize(host, port, key, freshness, alert_to, daemon_sleep)
+    def initialize(host, port, key_path, alert_to, daemon_sleep)
       @host = host
       @port = port
-      @key  = key
-      @freshness = freshness.to_i
+      @key_path = key_path
       @alert_to = alert_to
       @daemon_sleep = daemon_sleep.to_i
     end
@@ -21,15 +21,26 @@ module DeadmanCheck
       config.token = ENV['SLACK_API_TOKEN']
     end
 
-    def _diff_epoc(current_epoch, recorded_epoch)
+    def _diff_epoch(current_epoch, recorded_epoch)
       epoch_difference = current_epoch - recorded_epoch
       return epoch_difference
     end
 
-    def _get_recorded_epoch(host, port, key)
+    def _get_recorded_epochs(host, port, key)
       DeadmanCheck::DeadmanCheckGlobal.new.configure_diplomat(host, port)
-      recorded_epoch = Diplomat::Kv.get(key)
-      return recorded_epoch
+      recorded_epochs_json = Diplomat::Kv.get(key, recurse: true)
+      return recorded_epochs_json
+    end
+
+    def _check_recorded_epochs(recorded_epochs_json, current_epoch)
+      recorded_epochs_json.each do |recorded_service|
+        recorded_service.each do |service, value|
+          epoch_diff = _diff_epoch(value['epoch'], current_epoch)
+          if epoch_diff > value['frequency']
+            slack_alert(@alert_to, service, epoch_diff)
+          end
+        end
+      end
     end
 
     def slack_alert(alert_to, key, epoch_diff)
@@ -40,12 +51,9 @@ module DeadmanCheck
     end
 
     def run_check_once
-      recorded_epoch = _get_recorded_epoch(@host, @port, @key).to_i
+      recorded_epochs = _get_recorded_epochs(@host, @port, @key_path).to_i
       current_epoch = DeadmanCheck::DeadmanCheckGlobal.new.get_epoch_time.to_i
-      epoch_diff = _diff_epoc(current_epoch, recorded_epoch)
-      if epoch_diff > @freshness
-        slack_alert(@alert_to, @key, epoch_diff)
-      end
+      _check_recorded_epochs(recorded_epochs, current_epoch)
     end
 
     def run_check_daemon
